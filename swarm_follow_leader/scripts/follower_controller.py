fa9991553423b6b355bf5286a42005a65f33658f
hardlyrichie <hardlyrichie@gmail.com>
2020-12-07 11:22:04 -0500

    Lay out the core controller functions

--- swarm_follow_leader/scripts/follower_controller.py
+++ swarm_follow_leader/scripts/follower_controller.py
@@ -15,7 +15,30 @@ class Follower:
         rospy.Subscriber(f'/{robot_ns}/scan', LaserScan, self.process_scan)
 
     def process_scan(self, msg):
-        """ Process lidar scan data TODO: fill out with more accurate documentation """
+        """ Process lidar scan data and extracts distance measurements from left, right and front """
+        pass
+
+    def fuzzy_formation(self):
+        """ Fuzzy logic controller that determines the robot commands to keep in formation """
+        pass
+
+    def fuzzy_collision_avoidance(self):
+        """ Fuzzy logic controller that determines the robot commands to avoid obstacles """
+        pass
+
+    def fuzzy_fusion(self):
+        """ Fuzzy logic controller that combines formation and collision avoidance """
+        pass
+
+    def get_heading(self):
+        """ Determine heading of the follower in the world coordinate frame """
+        pass
+
+    def get_alpha(self):
+        """ Calculate angle between follower and leader (alpha) """
+        # TODO: Figure out how to use AR tags and computer vision to do this. If not possible, might
+        # have to fall back onto having to use odometry to relay leader position (x, y) and 
+        # calculate alpha to follower 
         pass
 
     def run(self):
