4d81e7050bf9399cacc095362c4c93d8fc2eac19
hardlyrichie <hardlyrichie@gmail.com>
2020-12-16 12:15:52 -0500

    Start formation engine

--- swarm_follow_leader/scripts/formation_engine.py
+++ swarm_follow_leader/scripts/formation_engine.py
@@ -1,18 +1,18 @@
-""" Code establishing the fuzzy logic controller for collision avoidance """
+""" Code establishing the fuzzy logic controller for staying in formation """
 
 import fuzzylite as fl
 
-avoidance_engine = fl.Engine(
-    name='collision_avoidance',
+formation_engine = fl.Engine(
+    name='formation',
     description=''
 )
 
-avoidance_engine.input_variables = [
+formation_engine.input_variables = [
     fl.InputVariable(
-        name='Left_Laser',
+        name='Angle',
         description='',
         enabled=True,
-        minimum=0.0, # The true range_min specified in sensor_msgs/LaserScan is ~0.1 
+        minimum=0.0,
         maximum=5.0,
         lock_range=True,
         terms=[
@@ -23,7 +23,7 @@ avoidance_engine.input_variables = [
         ]
     ),
     fl.InputVariable(
-        name='Right_Laser',
+        name='Distance',
         description='',
         enabled=True,
         minimum=0.0,
@@ -34,23 +34,10 @@ avoidance_engine.input_variables = [
             fl.Triangle('medium', 1.25, 2.5, 3.75),
             fl.Ramp('far', 2.5, 5)
         ]
-    ),
-    fl.InputVariable(
-        name='Front_Laser',
-        description='',
-        enabled=True,
-        minimum=0.0,
-        maximum=5.0,
-        lock_range=True,
-        terms=[
-            fl.Ramp('near', 2.5, 0),
-            fl.Triangle('medium', 1.25, 2.5, 3.75),
-            fl.Ramp('far', 2.5, 5)
-        ]
-    ),                        
+    ),                      
 ]
 
-avoidance_engine.output_variables = [
+formation_engine.output_variables = [
     fl.OutputVariable(
         name='Rotation',
         description='',
@@ -59,9 +46,8 @@ avoidance_engine.output_variables = [
         maximum=3,
         lock_range=True,
         aggregation=fl.Maximum(),
-        defuzzifier=fl.Centroid(), # Maybe play with the resolution of centroid?
+        defuzzifier=fl.Centroid(),
         terms=[
-            # Angular velocity (used be to degree of rotation mapped from -90 to 90 but /cmd_vel expects angular velocity)
             fl.Ramp('very_left', 1, 3),
             fl.Triangle('left', 0, 1, 3),
             fl.Triangle('straight_ahead', -1, 0, 1),
@@ -77,7 +63,7 @@ avoidance_engine.output_variables = [
         maximum=1,
         lock_range=True,
         aggregation=fl.Maximum(),
-        defuzzifier=fl.Centroid(), # Maybe play with the resolution of centroid?
+        defuzzifier=fl.Centroid(),
         terms=[
             fl.Ramp('reverse', 0, -.5),
             fl.Triangle('slow', -.05, .3, .6),
@@ -87,7 +73,7 @@ avoidance_engine.output_variables = [
     ),
 ]
 
-avoidance_engine.rule_blocks = [
+formation_engine.rule_blocks = [
     fl.RuleBlock(
         name="mamdani",
         description="",
@@ -99,79 +85,77 @@ avoidance_engine.rule_blocks = [
         rules=[
             # Left laser near section
             fl.Rule.create("if Left_Laser is near and Right_Laser is near and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is near and Front_Laser is medium \
-                            then Rotation is straight_ahead and Velocity is normal", avoidance_engine),
+                            then Rotation is straight_ahead and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is near and Front_Laser is far \
-                            then Rotation is straight_ahead and Velocity is fast", avoidance_engine),
+                            then Rotation is straight_ahead and Velocity is fast", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is medium and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is medium and Front_Laser is medium \
-                            then Rotation is right and Velocity is slow", avoidance_engine),
+                            then Rotation is right and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is medium and Front_Laser is far \
-                            then Rotation is very_right and Velocity is normal", avoidance_engine),
+                            then Rotation is very_right and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is far and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is far and Front_Laser is medium \
-                            then Rotation is very_right and Velocity is slow", avoidance_engine),
+                            then Rotation is very_right and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is near and Right_Laser is far and Front_Laser is far \
-                            then Rotation is very_right and Velocity is normal", avoidance_engine),
+                            then Rotation is very_right and Velocity is normal", formation_engine),
             # Left laser medium
             fl.Rule.create("if Left_Laser is medium and Right_Laser is near and Front_Laser is near \
-                            then Rotation is very_left and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_left and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is near and Front_Laser is medium \
-                            then Rotation is very_left and Velocity is slow", avoidance_engine),
+                            then Rotation is very_left and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is near and Front_Laser is far \
-                            then Rotation is very_left and Velocity is slow", avoidance_engine),
+                            then Rotation is very_left and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is medium and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is medium and Front_Laser is medium \
-                            then Rotation is right and Velocity is normal", avoidance_engine),
+                            then Rotation is right and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is medium and Front_Laser is far \
-                            then Rotation is very_right and Velocity is normal", avoidance_engine),
+                            then Rotation is very_right and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is far and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is far and Front_Laser is medium \
-                            then Rotation is very_right and Velocity is slow", avoidance_engine),
+                            then Rotation is very_right and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is medium and Right_Laser is far and Front_Laser is far \
-                            then Rotation is very_right and Velocity is normal", avoidance_engine),
+                            then Rotation is very_right and Velocity is normal", formation_engine),
             # Left laser far
             fl.Rule.create("if Left_Laser is far and Right_Laser is near and Front_Laser is near \
-                            then Rotation is very_left and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_left and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is near and Front_Laser is medium \
-                            then Rotation is very_left and Velocity is slow", avoidance_engine),
+                            then Rotation is very_left and Velocity is slow", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is near and Front_Laser is far \
-                            then Rotation is very_left and Velocity is normal", avoidance_engine),
+                            then Rotation is very_left and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is medium and Front_Laser is near \
-                            then Rotation is very_left and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_left and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is medium and Front_Laser is medium \
-                            then Rotation is left and Velocity is normal", avoidance_engine),
+                            then Rotation is left and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is medium and Front_Laser is far \
-                            then Rotation is left and Velocity is normal", avoidance_engine),
+                            then Rotation is left and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is far and Front_Laser is near \
-                            then Rotation is very_right and Velocity is reverse", avoidance_engine),
+                            then Rotation is very_right and Velocity is reverse", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is far and Front_Laser is medium \
-                            then Rotation is right and Velocity is normal", avoidance_engine),
+                            then Rotation is right and Velocity is normal", formation_engine),
             fl.Rule.create("if Left_Laser is far and Right_Laser is far and Front_Laser is far \
-                            then Rotation is straight_ahead and Velocity is normal", avoidance_engine),                                                                                                                                                                                                                                                                                                                                                                                             
+                            then Rotation is straight_ahead and Velocity is normal", formation_engine),                                                                                                                                                                                                                                                                                                                                                                                             
         ]
     )    
 ]
 
 if __name__ == "__main__":
     # Testing out the engine
-    left_laser = avoidance_engine.input_variable('Left_Laser')
-    right_laser = avoidance_engine.input_variable('Right_Laser')
-    front_laser = avoidance_engine.input_variable('Front_Laser')
+    angle = formation_engine.input_variable('Angle')
+    distance = formation_engine.input_variable('Distance')
 
-    rotation = avoidance_engine.output_variable('Rotation')
-    velocity = avoidance_engine.output_variable('Velocity')
+    rotation = formation_engine.output_variable('Rotation')
+    velocity = formation_engine.output_variable('Velocity')
 
-    left_laser.value = 2.5
-    right_laser.value = 2.5
-    front_laser.value = .1
+    angle.value = 2.5
+    distance.value = 2.5
 
-    avoidance_engine.process()
+    formation_engine.process()
 
     print('Rotation:', rotation.value)
     print('Velocity:', velocity.value)
